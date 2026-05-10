d0cf2dd24cbb33bd25a598fc1ed233f359a4c197
DeineMATRIX <neonator@hotmail.de>
2026-05-10 20:05:17 +0200

    Add files via upload

---
+++ team-data.js
@@ -0,0 +1,28 @@
+// team-data.js
+const TEAM_DATA = {
+    TEAM_NAME: "AirWaveBase",
+    TEAM_BESCHREIBUNG: "The first KICK Team and KICK DJ Team site.<br><br>We’re a collective of DJs from all over the world who are passionate about music and community, and we’re thrilled to have you as part of it. We cover all genres of music.<br><br>Feel free to check out each of us and discover both familiar favorites and new finds that you’ll love.<br><br>Join us! Boost your visibility and network across genres. Add our team logo with a link to our team page at the top section of your KICK About. That's all it takes if your channel meets the requirements. <a href='https://discord.gg' target='_blank'>Discord</a>",
+    
+    MITGLIEDER: [
+        "TeamMember_1",
+        "DeineMATRIX",
+        "TeamMember_2",
+        "TeamMember_3",
+        "holzi_music",
+        "Deejay_w",
+        "MarcoDeejayChannel",
+        "djp3itsch3",
+        "Sallyy_x",
+        "6hulksmash6",
+        "themysticmountainshow",
+        "drwolliakadrpfeffi",
+        "TeamMember_4",
+        "TeamMember_5",
+        "TeamMember_6",
+        "TeamMember_7",
+        "TeamMember_8",
+        "TeamMember_9",
+        "djtotygee",
+        "steffienova"
+    ]
+};
